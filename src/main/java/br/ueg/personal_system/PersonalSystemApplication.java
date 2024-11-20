4211fa440f19d77f0de77db66a846f30832e6d4f
Erik Mota <seriikmota.dev@gmail.com>
2024-11-20 20:15:59 -0300

    Initial commit

---
+++ src/main/java/br/ueg/personal_system/PersonalSystemApplication.java
@@ -0,0 +1,13 @@
+package br.ueg.personal_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalSystemApplication.class, args);
+	}
+
+}
