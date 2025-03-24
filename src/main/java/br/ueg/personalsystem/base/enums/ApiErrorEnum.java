0a314932ece2be583952b369facc2fc50b4e2af8
Erik Mota <seriikmota.dev@gmail.com>
2025-03-24 13:06:51 -0300

    Alterando código das mensagem de erro

--- src/main/java/br/ueg/personalsystem/base/enums/ApiErrorEnum.java
+++ src/main/java/br/ueg/personalsystem/base/enums/ApiErrorEnum.java
@@ -4,16 +4,16 @@ import lombok.Getter;
 
 @Getter
 public enum ApiErrorEnum implements MessageCode {
-    GENERAL("MEA001", br.ueg.personalsystem.base.enums.MessageType.ERROR),
-    NOT_FOUND("MEA002", br.ueg.personalsystem.base.enums.MessageType.ERROR),
-    PARAMETER_REQUIRED("MEA003", br.ueg.personalsystem.base.enums.MessageType.ERROR),
-    LOGIN_INVALID("MEA004", br.ueg.personalsystem.base.enums.MessageType.ERROR),
-    USER_PASSWORD_NOT_MATCH("MEA005", br.ueg.personalsystem.base.enums.MessageType.ERROR),
-    INACTIVE_USER("MEA006", br.ueg.personalsystem.base.enums.MessageType.ERROR),
-    INVALID_TOKEN("MEA007", br.ueg.personalsystem.base.enums.MessageType.ERROR),
-    EXPIRED_TOKEN("MEA008", br.ueg.personalsystem.base.enums.MessageType.ERROR),
-    MANDATORY_FIELD("MEA009", br.ueg.personalsystem.base.enums.MessageType.ERROR),
-    ACCESS_DENIED("MEA010", br.ueg.personalsystem.base.enums.MessageType.ERROR);
+    GENERAL("MESSAGE.GENERAL.M1", br.ueg.personalsystem.base.enums.MessageType.ERROR),
+    NOT_FOUND("MESSAGE.GENERAL.M2", br.ueg.personalsystem.base.enums.MessageType.ERROR),
+    PARAMETER_REQUIRED("MESSAGE.GENERAL.M3", br.ueg.personalsystem.base.enums.MessageType.ERROR),
+    LOGIN_INVALID("MESSAGE.LOGIN.M1", br.ueg.personalsystem.base.enums.MessageType.ERROR),
+    USER_PASSWORD_NOT_MATCH("MESSAGE.LOGIN.M2", br.ueg.personalsystem.base.enums.MessageType.ERROR),
+    INACTIVE_USER("MESSAGE.LOGIN.M3", br.ueg.personalsystem.base.enums.MessageType.ERROR),
+    INVALID_TOKEN("MESSAGE.LOGIN.M4", br.ueg.personalsystem.base.enums.MessageType.ERROR),
+    EXPIRED_TOKEN("MESSAGE.LOGIN.M5", br.ueg.personalsystem.base.enums.MessageType.ERROR),
+    MANDATORY_FIELD("MESSAGE.GENERAL.M4", br.ueg.personalsystem.base.enums.MessageType.ERROR),
+    ACCESS_DENIED("MESSAGE.LOGIN.M6", br.ueg.personalsystem.base.enums.MessageType.ERROR);
 
     private final String code;
     private final br.ueg.personalsystem.base.enums.MessageType type;
