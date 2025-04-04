d7df148e7f133d8d6887ee1c2db1917629350a1a
davif <davifariagoncalves@gmail.com>
2025-04-04 11:20:42 -0300

    fix/ajuste dto list anamnese

--- src/main/java/br/ueg/personalsystem/dto/list/AnamneseListDTO.java
+++ src/main/java/br/ueg/personalsystem/dto/list/AnamneseListDTO.java
@@ -1,6 +1,7 @@
 package br.ueg.personalsystem.dto.list;
 
 
+import br.ueg.personalsystem.base.annotation.MandatoryField;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,9 +15,30 @@ import java.time.LocalDate;
 @AllArgsConstructor
 public class AnamneseListDTO {
    private Long id;
+
    private PatientListDTO patient;
+
    private LocalDate anamnesisDate;
+
+   private String mainComplaints;
+
+   private String medicalHistory;
+
+   private String observations;
+
    private Double weight;
+
    private Double height;
+
+   private Double waistCircumference;
+
+   private Double hipCircumference;
+
+   private Double bodyFatPercentage;
+
+   private Double muscleMass;
+
    private Double bodyMassIndex;
+
+   private Double waistHipRatio;
 }
