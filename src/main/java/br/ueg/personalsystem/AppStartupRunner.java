0397dcd8dc5c6f2a98d300908526810bf336603f
Erik Mota <seriikmota.dev@gmail.com>
2025-04-29 17:24:42 -0300

    commit

--- src/main/java/br/ueg/personalsystem/AppStartupRunner.java
+++ src/main/java/br/ueg/personalsystem/AppStartupRunner.java
@@ -13,8 +13,10 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
+import java.security.*;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Base64;
 import java.util.List;
 
 @Component
@@ -30,7 +32,7 @@ public class AppStartupRunner implements ApplicationRunner {
     @Autowired
     private UserGroupRepository userGroupRepository;
 
-    public void initData() {
+    public void initData() throws NoSuchAlgorithmException {
         if (userRepository.count() == 0 && roleRepository.count() == 0 && userGroupRepository.count() == 0) {
             List<String> roles = Arrays.asList(
                     "ROLE_USER_CREATE",
@@ -66,6 +68,16 @@ public class AppStartupRunner implements ApplicationRunner {
                     .roles(roleList)
                     .build();
 
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+            keyPairGenerator.initialize(2048);
+
+            KeyPair keyPair = keyPairGenerator.generateKeyPair();
+            PublicKey publicKey = keyPair.getPublic();
+            PrivateKey privateKey = keyPair.getPrivate();
+
+            String publicKeyBase64 = Base64.getEncoder().encodeToString(publicKey.getEncoded());
+            String privateKeyBase64 = Base64.getEncoder().encodeToString(privateKey.getEncoded());
+
             User user = User.builder()
                     .name("Administrador")
                     .login("admin")
@@ -73,8 +85,8 @@ public class AppStartupRunner implements ApplicationRunner {
                     .email("teste@gmail.com")
                     .enabled(Boolean.TRUE)
                     .userGroup(userGroup)
-//                    .apiKeyEvolution("3DBFB2F3-AD91-4ABC-9ED3-4072027EC101")
-//                    .instanceNameEvolution("Instancia Erik")
+                    .publicKey(publicKeyBase64)
+                    .privateKey(privateKeyBase64)
                     .build();
 
             roleRepository.saveAll(roleList);
